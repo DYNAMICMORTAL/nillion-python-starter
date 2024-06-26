e99df84c28c5fa260813af86e87bccc3ed4cb8b1
DYNAMICMORTAL <aminvasudev6@gmail.com>
2024-06-26 05:26:29 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,19 @@
 from nada_dsl import *
 
-
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    parties = [Party(name=f"Party{i+1}") for i in range(5)]
+ 
+    bids = [SecretInteger(Input(name=f"bid_{party.name}", party=party)) for party in parties]
+    max_limits = [SecretInteger(Input(name=f"max_limit_{party.name}", party=party)) for party in parties]
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    valid_bids = [IfElse(bid <= max_limit, bid, max_limit) for bid, max_limit in zip(bids, max_limits)]
 
-    return [Output(my_int1, "my_output", party1)]
+    highest_bid = SecretInteger(0)
+    winner = SecretString("No winner")
+
+    for i, bid in enumerate(valid_bids):
+        is_higher = bid > highest_bid
+        highest_bid = IfElse(is_higher, bid, highest_bid)
+        winner = IfElse(is_higher, parties[i].name, winner)
+    return [Output(highest_bid, "highest_bid", parties[0]), Output(winner, "winner", parties[0])]
+
